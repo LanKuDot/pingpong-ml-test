edc1c35af6042dc5be7a169cbafd863c26c3f187
Kun-Yi Li <LanKuDot@users.noreply.github.com>
2020-04-21 16:54:04 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -1,18 +1,15 @@
-import games.pingpong.communication as comm
-from games.pingpong.communication import (
-    SceneInfo, GameStatus, PlatformAction
-)
+from mlgame.communication import ml as comm
 
 def ml_loop(side: str):
     print("For {}".format(side))
     comm.ml_ready()
 
     while True:
-        scene_info = comm.get_scene_info()
+        scene_info = comm.recv_from_game()
 
-        if scene_info.status == GameStatus.GAME_1P_WIN or \
-           scene_info.status == GameStatus.GAME_2P_WIN:
+        if scene_info.status == "GAME_1P_WIN" or \
+           scene_info.status == "GAME_2P_WIN":
             comm.ml_ready()
             continue
 
-        comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+        comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_LEFT"})
